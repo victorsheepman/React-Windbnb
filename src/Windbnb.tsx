17fc157c74c0a68b75fb5fcb669e2597dd7f428d
victor_marquez <victorsheepman@gmail.com>
2023-12-16 17:37:34 -0400

    remove erroe

--- src/Windbnb.tsx
+++ src/Windbnb.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+
 import { Header } from './containers/Header'
 import { MainStay } from './containers/MainStay'
 import { WindbnbContextProvider } from './context/WindbnbContext'
-//import data from '../data'
+
 export const Windbnb = () => {
   return (
     <WindbnbContextProvider>
--- src/assets/SearchIcon.tsx
+++ src/assets/SearchIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 export const SearchIcon = ({color="red"}:{color:string}) => {
   return (
